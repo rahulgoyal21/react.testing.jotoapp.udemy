ca3bab7c526b87e554b6e1265fa161c5c8e615ba
Rahul Goyal <rahulgoyal7480@gmail.com>
2020-10-24 13:41:09 +0530

    Added:Guessed words tests

---
+++ src/GuessedWords.test.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { findByTestAttr, checkProps } from '../test/testUtils';
+import GuessedWords from './GuessedWords';
+
+const defaultProps = {
+  guessedWords: [{ guessedWord: 'train', letterMatchCount: 3 }]
+};
+
+/**
+ * Factory function to create a shallowWrapper for the GuessedWords component.
+ * @function setup
+ * @param {object} props - Component props specific to this setup.
+ * @returns {ShallowWrapper}
+ */
+const setup = (props = {}) => {
+  const setupProps = { ...defaultProps, ...props };
+  return shallow(<GuessedWords {...setupProps} />);
+};
+
+test('Does not throw warning with expected props', () => {
+  checkProps(GuessedWords, defaultProps);
+});
+
+//Describe is a way of groupig tests
+describe('if there are no words guessed', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = setup({ guessedWords: [] });
+  });
+  test('renders without error', () => {
+    const component = findByTestAttr(wrapper, 'component-guessed-words');
+    expect(component.length).toBe(1);
+  });
+  test('renders instructions to guess a word', () => {
+    const instructions = findByTestAttr(wrapper, 'guess-instructions');
+    expect(instructions.text().length).not.toBe(0);
+  });
+});
+describe('if there are words guessed', () => {});
