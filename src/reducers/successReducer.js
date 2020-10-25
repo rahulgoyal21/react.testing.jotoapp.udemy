e549c0f54b2e46834e08c55463472bd6d9c9b721
Rahul Goyal <rahulgoyal7480@gmail.com>
2020-10-25 13:43:59 +0530

    Added:successREducer tests

--- src/reducers/successReducer.js
+++ src/reducers/successReducer.js
@@ -1,10 +1,17 @@
+import { actionTypes } from '../actions';
 /**
  * @function successReducer
- * @param {array} state - Array of guessed words.
+ * @param {boolean} state - Array of guessed words.
  * @param {object} action - Action to be reduced.
  * @returns {boolean} - new success state
  */
 
-export default (state, action) => {
-  return null;
+export const successReducer = (state = false, action) => {
+  const { type } = action;
+  switch (type) {
+    case actionTypes.CORRECT_GUESS:
+      return true;
+    default:
+      return state;
+  }
 };
---
+++ src/reducers/successReducer.test.js
@@ -0,0 +1,14 @@
+import { successReducer } from './successReducer';
+import { actionTypes } from '../actions';
+
+test('returns default initial state of `false` when no action passed', () => {
+  const newState = successReducer(undefined, {});
+  expect(newState).toBe(false);
+});
+
+test('returns state of true upon receiving an action of type `CORRECT_GUESS`', () => {
+  const newState = successReducer(undefined, {
+    type: actionTypes.CORRECT_GUESS
+  });
+  expect(newState).toBe(true);
+});
