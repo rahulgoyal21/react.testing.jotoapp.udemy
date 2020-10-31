2b9a1f8c3da638d63cb0974eea67f89351a5e5b9
Rahul Goyal <rahulgoyal7480@gmail.com>
2020-10-31 19:00:27 +0530

    Added:connected components setup

---
+++ src/Input.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+const Input = () => {
+  return (
+    <div>
+      <button />
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => ({});
+
+export default connect(mapStateToProps)(Input);
--- test/testUtils.js
+++ test/testUtils.js
@@ -1,4 +1,17 @@
 import checkPropTypes from 'check-prop-types';
+import { createStore } from 'redux';
+import rootReducer from '../src/reducers';
+
+/**
+ * Create a testing store with imported reducers, middleware, and initial state.
+ * @param {object} initialState - Initial state for store
+ * @function storeFactory
+ * @returns {Store}- Redux store
+ */
+
+export const storeFactory = (initialState) => {
+  return createStore(rootReducer, initialState);
+};
 
 /**
  * Returns nodes with the given data-test attribute
