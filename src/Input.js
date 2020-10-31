c139e7830063fc63386e9e2791f1a5a0e6b8c4fa
Rahul Goyal <rahulgoyal7480@gmail.com>
2020-10-31 19:20:30 +0530

    Added:connected component test

--- src/Input.js
+++ src/Input.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-const Input = () => {
-  return (
-    <div>
-      <button />
-    </div>
+const Input = ({ success }) => {
+  const contents = success ? null : (
+    <form className='form-inline'>
+      <input
+        data-test='input-box'
+        className='mb-2 mx-sm-3'
+        type='text'
+        placeholder='enter guess'
+      />
+      <button
+        data-test='submit-button'
+        className='btn btn-primary mb-2'
+        type='submit'
+      >
+        Submit
+      </button>
+    </form>
   );
+  return <div data-test='component-input'> {contents}</div>;
 };
 
-const mapStateToProps = (state) => ({});
+const mapStateToProps = (state) => ({
+  success: state.successReducer
+});
 
 export default connect(mapStateToProps)(Input);
