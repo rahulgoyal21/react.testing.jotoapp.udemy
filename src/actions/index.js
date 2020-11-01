836cbcc0eb7eb07453b4442a930b1678ef0bd243
Rahul Goyal <rahulgoyal7480@gmail.com>
2020-11-01 14:34:28 +0530

    Added:integration testing

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,33 @@
+import { getLetterMatchCount } from '../helpers';
 export const actionTypes = {
-  CORRECT_GUESS: 'CORRECT_GUESS'
+  CORRECT_GUESS: 'CORRECT_GUESS',
+  GUESS_WORD: 'GUESS_WORD'
 };
 
+// /**
+//  * @function correctGuess
+//  * @returns {object} - Action object with type 'CORRECT_GUESS'
+//  */
+// export const correctGuess = () => {
+//   return { type: actionTypes.CORRECT_GUESS };
+// };
+
 /**
- * @function correctGuess
- * @returns {object} - Action object with type 'CORRECT_GUESS'
+ * Returns Redux Thunk function that dispathces GUESS_WORD action and (conditionally) CORRECT_GUESS action
+ *@function guessWord
+ * @param {string} guessedWord - Guessed Word.
+ * @returns {function} - Redux Thunk Function.
  */
-export const correctGuess = () => {
-  return { type: actionTypes.CORRECT_GUESS };
+export const guessWord = (guessedWord) => {
+  return function (dispatch, getState) {
+    const secretWord = getState().secretWord;
+    const letterMatchCount = getLetterMatchCount(guessedWord, secretWord);
+    dispatch({
+      type: actionTypes.GUESS_WORD,
+      payload: { guessedWord, letterMatchCount }
+    });
+    if (guessedWord === secretWord) {
+      dispatch({ type: actionTypes.CORRECT_GUESS });
+    }
+  };
 };
