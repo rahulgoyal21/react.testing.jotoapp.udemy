02ed574e31a24cad5baa0d80863cf6164d897a1a
Rahul Goyal <rahulgoyal7480@gmail.com>
2020-11-15 14:58:45 +0530

    App component's redux props testing

--- src/App.js
+++ src/App.js
@@ -2,19 +2,25 @@ import React, { Component } from 'react';
 import './App.css';
 import Congrats from './Congrats';
 import GuessedWords from './GuessedWords';
+import { connect } from 'react-redux';
+import { getSecretWord } from './actions';
+import Input from './Input';
 
 class App extends Component {
   render() {
     return (
       <div className='container'>
         <h1>Jotto</h1>
-        <Congrats success={true} />
-        <GuessedWords
-          guessedWords={[{ guessedWord: 'train', letterMatchCount: 3 }]}
-        />
+        <Congrats success={this.props.success} />
+        <Input />
+        <GuessedWords guessedWords={this.props.guessedWords} />
       </div>
     );
   }
 }
-
-export default App;
+const mapStateToProps = (state) => ({
+  success: state.success,
+  guessedWords: state.guessedWords,
+  secretWord: state.secretWord
+});
+export default connect(mapStateToProps, { getSecretWord })(App);
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,36 @@
 import React from 'react';
-import Enzyme, { shallow } from 'enzyme';
-import EnzymeAdapter from 'enzyme-adapter-react-16';
+import { shallow } from 'enzyme';
+import { storeFactory } from '../test/testUtils';
 import App from './App';
 
-Enzyme.configure({ adapter: new EnzymeAdapter() });
+const setup = (state = {}) => {
+  const store = storeFactory(state);
+  const wrapper = shallow(<App store={store} />).dive();
+  return wrapper;
+};
 
-test('renders learn react link', () => {});
+describe('redux properties', () => {
+  test('has access to "success" state', () => {
+    const success = true;
+    const wrapper = setup({ success });
+    const successProp = wrapper.props().success;
+    expect(successProp).toBe(success);
+  });
+  test('has access to "secretWord" state', () => {
+    const secretWord = 'party';
+    const wrapper = setup({ secretWord });
+    const secretWordProp = wrapper.props().secretWord;
+    expect(secretWordProp).toBe(secretWord);
+  });
+  test('has access to "guessedWords" state', () => {
+    const guessedWords = [{ guessedWord: 'train', letterMatchCount: 3 }];
+    const wrapper = setup({ guessedWords });
+    const guessedWordsProp = wrapper.props().guessedWords;
+    expect(guessedWordsProp).toEqual(guessedWords);
+  });
+  test('"getSecretWord" action creator is a function on the props', () => {
+    const wrapper = setup();
+    const getSecretWordProp = wrapper.props().getSecretWord;
+    expect(getSecretWordProp).toBeInstanceOf(Function);
+  });
+});
